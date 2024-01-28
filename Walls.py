e8ddd46e9e6deabdd33f94c358e4b55916562824
ndf17a <ndf17a@acu.edu>
2024-01-28 02:44:44 -0600

    lots of changes dont ask

--- Walls.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Wall:
-
-    def __init__(self, x, y, width, height):
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(x, y, width, height)
---
+++ buttons/InfoBlock.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.locals import *
+
+class StatBlock:
+    def __init__(self, block, sellBlock):
+        self.block = block
+        self.sellBlock = sellBlock
+
+
+
+
+
+
